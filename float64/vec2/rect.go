6555124982724326a21201ba8d4fbbbb758e08f8
aninggo <aninggo@gmail.com>
2021-05-15 16:55:40 +0800

    update

--- float64/vec2/rect.go
+++ float64/vec2/rect.go
@@ -2,6 +2,7 @@ package vec2
 
 import (
 	"fmt"
+	"math"
 )
 
 // Rect is a coordinate system aligned rectangle defined by a Min and Max vector.
@@ -21,6 +22,20 @@ func NewRect(a, b *T) (rect Rect) {
 func ParseRect(s string) (r Rect, err error) {
 	_, err = fmt.Sscan(s, &r.Min[0], &r.Min[1], &r.Max[0], &r.Max[1])
 	return r, err
+}
+
+func (rect *Rect) Width() float64 {
+	return rect.Min[0] - rect.Max[0]
+}
+
+func (rect *Rect) Height() float64 {
+	return rect.Min[1] - rect.Max[1]
+}
+
+func (rect *Rect) Size() float64 {
+	width := b.Width()
+	height := b.Height()
+	return math.Max(width, height)
 }
 
 // Slice returns the elements of the vector as slice.
--- vec2/rect.go
+++ vec2/rect.go
@@ -2,6 +2,7 @@ package vec2
 
 import (
 	"fmt"
+	"math"
 )
 
 // Rect is a coordinate system aligned rectangle defined by a Min and Max vector.
@@ -14,6 +15,20 @@ type Rect struct {
 func ParseRect(s string) (r Rect, err error) {
 	_, err = fmt.Sscan(s, &r.Min[0], &r.Min[1], &r.Max[0], &r.Max[1])
 	return r, err
+}
+
+func (rect *Rect) Width() float32 {
+	return rect.Min[0] - rect.Max[0]
+}
+
+func (rect *Rect) Height() float32 {
+	return rect.Min[1] - rect.Max[1]
+}
+
+func (rect *Rect) Size() float32 {
+	width := b.Width()
+	height := b.Height()
+	return math.Max(width, height)
 }
 
 // Slice returns the elements of the vector as slice.
